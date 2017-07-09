6be027172bcc9d50ea1c30f24e4af992f7912a1c
DESKTOP-BBKBQMF\Marnee <marnee.dearman@gmail.com>
2017-07-09 13:33:33 -0700

    my hellow world example for awd deployment using the CLI

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+import boto3
+
+s3_client = boto3.client("s3")
+
+def lambda_handler(event, context):
+    print ("The sleeper has awakened.")
+    return "The sleeper has awakened."

0704380c2882584cb3d3d3ae7a0a418c704d8bdd
DESKTOP-BBKBQMF\Marnee <marnee.dearman@gmail.com>
2017-07-14 20:31:07 -0700

    Cleaned up the code a bit and more better messages.

--- functions/s3event.py
+++ functions/s3event.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import json
-# from urllib.parse import unquote_plus
 import boto3
+import datetime
 
 print('Loading function')
 
@@ -9,23 +9,21 @@ s3_client = boto3.client('s3')
 s3_resouce = boto3.resource('s3')
 
 def lambda_handler(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
-    print("Context: ")
-    print(context)
+    print("Received event: %s" % json.dumps(event, indent=2))
+    print("Context: %s" % context)
 
     # Get the object from the event and show its content type
     bucket = event['Records'][0]['s3']['bucket']['name']
     print (bucket)
-    # key = unquote_plus(event['Records'][0]['s3']['object']['key']
-        # .encode('utf8'))
     key = event['Records'][0]['s3']['object']['key']
     try:
         response = s3_client.get_object(Bucket=bucket, Key=key)
         print("CONTENT TYPE: " + response['ContentType'])
         # write to the target bucket
         # tuple-target
-        s3_resouce.Object("tuple-target", "s3event-results.txt").put(Body=response["ContentType"])
-        return response['ContentType']
+        target_message = "The MIME-type is : " + response["ContentType"] + ". At " + datetime.datetime.now
+        s3_resouce.Object("tuple-target", "s3event-results.txt").put(target_message)
+        return "I wrote to the file in tuple-target. " + target_message
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure it exists and your bucket is in the same region as this function.'.format(key, bucket))
